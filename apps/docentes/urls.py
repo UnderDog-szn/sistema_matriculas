60c281352215260d9bb1c8c669da9755351c083c
UnderDog_Szn <fadiaz@uniguajira.edu.co>
2026-06-20 19:43:37 -0500

    Métodos en los módelos actualizados

--- apps/docentes/urls.py
+++ apps/docentes/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('crear-docente/', views.crear_docente, name='crear_docente'),
-    path('lista-docentes/', views.lista_docentes, name='lista_docentes'),
+    path('', views.lista_docentes, name='lista_docentes'),
+    path('<str:pk>/editar/', views.actualizar_docente, name='actualizar_docente'),
+    path('<str:pk>/eliminar/', views.eliminar_docente, name='eliminar_docente'),
 ]
--- apps/docentes/views.py
+++ apps/docentes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .forms import DocenteForm
 from .models import docentes
 
@@ -19,7 +19,30 @@ def crear_docente(request):
         {'form_docente': form, 'ultimos_docentes': ultimos_docentes},
     )
 
-
+# ===== INICIO: Leer Docentes =====
 def lista_docentes(request):
     docentes_lista = docentes.objects.all()
     return render(request, 'docentes/lista_docentes.html', {'docentes': docentes_lista})
+# ===== FIN: Leer Docentes =====
+
+# ===== INICIO: Actualizar Docente =====
+def actualizar_docente(request, pk):
+    docente = get_object_or_404(docentes, pk=pk)
+    if request.method == 'POST':
+        form = DocenteForm(request.POST, instance=docente)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_docentes')
+    else:
+        form = DocenteForm(instance=docente)
+    return render(request, 'docentes/inicio.html', {'form_docente': form})
+# ===== FIN: Actualizar Docente =====
+
+# ===== INICIO: Eliminar Docente =====
+def eliminar_docente(request, pk):
+    docente = get_object_or_404(docentes, pk=pk)
+    if request.method == 'POST':
+        docente.delete()
+        return redirect('lista_docentes')
+    return render(request, 'docentes/confirmar_eliminar.html', {'docente': docente})
+# ===== FIN: Eliminar Docente =====
